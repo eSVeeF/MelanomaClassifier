6d45510e9f6e8f118b920a2d9369baa6a11413c2
eSVeeF <151464910+eSVeeF@users.noreply.github.com>
2023-11-30 17:24:01 -0500

    Two new features

---
+++ FeatureBuilders/RatioAreaPerimeterBuilder.py
@@ -0,0 +1,25 @@
+import numpy as np
+import importlib
+
+FEATURE_NAME = "RatioAreaPerimeter"
+READY = True
+IMAGE_TYPE = "LESION"  # Options: "NORMAL", "LESION", "BOTH"
+
+def build(image_normal=None, image_lesion=None):
+    if IMAGE_TYPE == "NORMAL":
+        return calculate_feature(image_normal)
+    elif IMAGE_TYPE == "LESION":
+        return calculate_feature(image_lesion)
+    elif IMAGE_TYPE == "BOTH":
+        return combine_features(image_normal, image_lesion)
+
+def calculate_feature(image):
+    module_area = importlib.import_module(f'FeatureBuilders.AreaBuilder')
+    module_perimeter = importlib.import_module(f'FeatureBuilders.PerimeterBuilder')
+    feature_area = module_area.build(image_lesion=image)
+    feature_perimeter = module_perimeter.build(image_lesion=image)
+    return feature_area/feature_perimeter
+
+def combine_features(image_normal, image_lesion):
+    # Implement logic to combine features from both image types
+    pass
