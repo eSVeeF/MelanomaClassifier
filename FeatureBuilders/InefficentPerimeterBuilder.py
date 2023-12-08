7b78516a508752dc9cd691f2ae533714d8dff7ac
eSVeeF <151464910+eSVeeF@users.noreply.github.com>
2023-12-08 14:10:02 -0500

    Rename PerimeterBuilder.py to InefficentPerimeterBuilder.py

--- FeatureBuilders/InefficentPerimeterBuilder.py
+++ FeatureBuilders/InefficentPerimeterBuilder.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-FEATURE_NAME = "Perimeter"
-READY = True
+FEATURE_NAME = "InefficientPerimeter"
+READY = False
 IMAGE_TYPE = "LESION"  # Options: "NORMAL", "LESION", "BOTH"
 
 def build(image_normal=None, image_lesion=None):
