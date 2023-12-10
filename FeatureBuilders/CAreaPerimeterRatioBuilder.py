bd3c1fb6f83b41ec6d7d6e29484d13075ca233ea
eSVeeF <151464910+eSVeeF@users.noreply.github.com>
2023-12-10 15:01:50 -0500

    Update CAreaPerimeterRatioBuilder.py

--- FeatureBuilders/CAreaPerimeterRatioBuilder.py
+++ FeatureBuilders/CAreaPerimeterRatioBuilder.py
@@ -2,7 +2,7 @@ import numpy as np
 import scipy.ndimage
 
 FEATURE_NAME = "PerimeterRatioAreaBuilder"
-READY = True
+READY = False
 IMAGE_TYPE = "LESION"  # Options: "NORMAL", "LESION", "BOTH"
 
 def build(image_normal=None, image_lesion=None):
