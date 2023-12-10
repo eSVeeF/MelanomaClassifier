282d7415c244475d23e11571b5854b50fa5d2a0b
eSVeeF <151464910+eSVeeF@users.noreply.github.com>
2023-12-09 21:14:24 -0500

    1D_10fold_CNN.py

---
+++ 1D_10fold_CNN.py
@@ -0,0 +1,142 @@
+import numpy as np
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import Conv1D, MaxPooling1D, Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from keras.metrics import Recall
+from keras.optimizers import Adam
+from read_images import ImageLoader
+import os
+import importlib
+from sklearn.model_selection import train_test_split, KFold
+from keras.callbacks import EarlyStopping
+
+# Load dataset
+dataset = pd.read_csv('mod_PH2_dataset.csv')
+dataset['Name'] = dataset['Name'].str.strip() + '.bmp'
+labels_dict = dict(zip(dataset['Name'], dataset['Label']))
+
+# Load images from both directories
+relative_dir = 'PH2Dataset'
+image_loader_normal = ImageLoader(relative_dir + '/Custom Images/Normal')
+image_loader_lesion = ImageLoader(relative_dir + '/Custom Images/Lesion')
+labels = [labels_dict[image_loader_normal.bmp_files[i]] for i in range(len(image_loader_normal.bmp_files))]
+
+# use target_size=(761, 553) in read_images.py
+# Feature Builders loading
+feature_builders = []
+for file in os.listdir('FeatureBuilders'):
+    if file.endswith('.py'):
+        module_name = file[:-3]
+        module = importlib.import_module(f'FeatureBuilders.{module_name}')
+        if getattr(module, 'READY', False):
+            print("Loaded " + getattr(module, 'FEATURE_NAME', False) + " builder")
+            feature_builders.append(module)
+
+print("Loaded " + str(len(feature_builders)) + " feature builders")
+# Feature extraction
+features = []
+for normal_image, lesion_image in zip(image_loader_normal.images_arrays, image_loader_lesion.images_arrays):
+    flattened_image = normal_image.flatten()
+    reduced_image = []
+    for builder in feature_builders:
+        image_type = getattr(builder, 'IMAGE_TYPE', 'NORMAL')
+        if image_type == 'NORMAL':
+            feature = builder.build(image_normal=normal_image)
+        elif image_type == 'LESION':
+            feature = builder.build(image_lesion=lesion_image)
+        elif image_type == 'BOTH':
+            feature = builder.build(normal_image, lesion_image)
+        reduced_image = np.append(reduced_image, feature)
+    features.append(reduced_image)
+
+
+#Normalize
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+# Apply MinMaxScaler to the last three variables
+scaler = MinMaxScaler()
+
+features = list(scaler.fit_transform(features))
+
+# Convert features and labels to NumPy arrays
+X = np.array(features)
+y = np.array(labels)
+
+import random
+# Prepare data for ML model
+rand = random.randint(0,100)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=rand)
+
+# Ensure labels are the correct shape
+y_train = y_train.reshape(-1, 1)
+y_test = y_test.reshape(-1, 1)
+
+# Define the input shape based on the reshaped data
+n_features = X_train.shape[1]
+input_shape = (n_features, 1)
+
+num_folds = 10
+kfold = KFold(n_splits=num_folds, shuffle=True)
+
+# List to store results from each fold
+fold_results = []
+
+# K-fold Cross Validation model evaluation
+fold_no = 1
+for train, test in kfold.split(X, y):
+    cnn_model = Sequential([
+        Conv1D(32, kernel_size=2, activation='relu', input_shape=input_shape),
+        Dropout(0.25),
+
+        Conv1D(64, kernel_size=2, activation='relu'),
+        Dropout(0.25),
+
+        Flatten(),
+        Dense(128, activation='relu'),
+        Dropout(0.5),
+        Dense(1, activation='sigmoid')  # Use 'softmax' if you have more than two classes
+    ])
+
+    # Compile the model
+    cnn_model.compile(loss='binary_crossentropy',
+                      optimizer=Adam(learning_rate=0.001),
+                      metrics=['accuracy', Recall()])
+
+    # Ensure labels are the correct shape for the fold
+    y_train_fold = y[train].reshape(-1, 1)
+    y_test_fold = y[test].reshape(-1, 1)
+
+    print('------------------------------------------------------------------------')
+    print(f'Training for fold {fold_no} ...')
+
+    early_stopping = EarlyStopping(monitor='loss', patience=10, mode='min')
+
+    X_train_reshaped = X_train.reshape(X_train.shape[0], X_train.shape[1], 1)
+    X_test_reshaped = X_test.reshape(X_test.shape[0], X_test.shape[1], 1)
+
+    # Fit data to model
+    history = cnn_model.fit(X[train], y[train].reshape(-1, 1),
+                            batch_size=32,
+                            epochs=100,
+                            verbose=1,
+                            callbacks=[early_stopping])
+
+    # Evaluate the model on the test data
+    scores = cnn_model.evaluate(X[test], y[test].reshape(-1, 1), verbose=0)
+
+    # Append the scores to the results list
+    fold_results.append(scores)
+
+    print(
+        f'Score for fold {fold_no}: {cnn_model.metrics_names[0]} of {scores[0]}; {cnn_model.metrics_names[1]} of {scores[1] * 100}%; {cnn_model.metrics_names[2]} of {scores[2] * 100}%')
+
+    fold_no += 1
+
+print(fold_results)
+
+average_results = np.mean(fold_results, axis=0)
+std_dev_results = np.std(fold_results, axis=0)
+print('------------------------------------------------------------------------')
+print('Average scores for all folds:')
+for i in range(len(cnn_model.metrics_names)):
+    print(f'{cnn_model.metrics_names[i]}: {average_results[i]} (± {std_dev_results[i]})')
